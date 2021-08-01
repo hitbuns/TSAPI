dd1f9537f6ef9dbc307a19ca83a529a84f66d206
hitbuns <hitbuns@gmail.com>
2021-08-01 19:18:34 +1000

    Added TabList Header and Footer Updates

--- src/com/PacketAnal/TSAPI.java
+++ src/com/PacketAnal/TSAPI.java
@@ -6,10 +6,11 @@ import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.Collection;
 
-public class TSAPI extends JavaPlugin{
+public class TSAPI extends JavaPlugin {
 
     public void sendActionbar(String message,Player... players) {
         if (message == null) return;
@@ -33,6 +34,24 @@ public class TSAPI extends JavaPlugin{
             return;
         }
         send(Arrays.asList(players),titlePacket,subTitlePacket);
+    }
+
+    public void updateTabHeader(String header,String footer,Player... players) {
+        PacketPlayOutPlayerListHeaderFooter packetPlayOutPlayerListHeaderFooter =
+                new PacketPlayOutPlayerListHeaderFooter(new ChatComponentText(Utils.color(header)));
+        try {
+            Field field = PacketPlayOutPlayerListHeaderFooter.class.getDeclaredField("b");
+            field.setAccessible(true);
+            field.set(packetPlayOutPlayerListHeaderFooter,new ChatComponentText(Utils.color(footer)));
+            field.setAccessible(false);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        if (players == null || players.length == 0) {
+            send(Bukkit.getOnlinePlayers(),packetPlayOutPlayerListHeaderFooter);
+            return;
+        }
+        send(Arrays.asList(players),packetPlayOutPlayerListHeaderFooter);
     }
 
     private void send(Collection<? extends Player> players,Packet<PacketListenerPlayOut>... packets) {
