e43a112bd67302c077065b063702fb60e002be2f
hitbuns <hitbuns@gmail.com>
2021-08-05 21:09:04 +1000

    Fixed NullPointerException at Utils

--- src/com/PacketAnal/Utils.java
+++ src/com/PacketAnal/Utils.java
@@ -6,8 +6,8 @@ import org.bukkit.ChatColor;
 public class Utils {
 
     public static String color(@NotNull String s) {
-        return ChatColor
-                .translateAlternateColorCodes('&',s);
+        return (s != null) ? ChatColor
+                .translateAlternateColorCodes('&',s):null;
     }
 
 }
